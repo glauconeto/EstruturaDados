172e5b4764681bff03cfc936c9cc6d9ebc4ba950
glauconeto <glauco.neto@outlook.com>
2023-10-12 17:58:59 -0300

    Métodos diferentes para adicionar elementos.

--- src/com/glauco/cursoestruturas/array/Array.java
+++ src/com/glauco/cursoestruturas/array/Array.java
@@ -9,6 +9,8 @@ public class Array {
         this.tamanho = 0;
     }
 
+    // Método que funciona, porém apenas adiciona a quantidade de elementos informados,
+    // podendo haver elementos vagos.
     /* public void adiciona(String elemento) {
         for (int i = 0; i < this.elementos.length; i++) {
             if (this.elementos[i] == null) {
@@ -18,5 +20,26 @@ public class Array {
         }
     } */
 
+    // Método que verifica se o tamanho do array é maior que a capacidade do array.
+    /* public boolean function adiciona(String elemento) {
+        if (this.tamanho < this.elementos.length) {
+            this.elementos[this.tamanho] = elemento;
+            this.tamanho++;
 
+            return true;
+        }
+        
+        return false;
+    } */
+
+    // Método que verifica se o tamanho do array é maior que a capacidade do array.
+    public void function adiciona(String elemento) {
+        if (this.tamanho < this.elementos.length) {
+            this.elementos[this.tamanho] = elemento;
+            this.tamanho++;
+        } else {
+            throw new Exception("Array já cheio, não é possível adicionar novos elementos!");
+        }
+
+    }
 }
