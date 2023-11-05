2f64d320c1129d28b9f4333212c6249fc0ddb4eb
Glauco Neto <glauco.neto@outlook.com>
2023-11-05 10:23:57 -0300

    Classe array list

---
+++ src/com/glauco/estruturadados/array/Aula12.java
@@ -0,0 +1,39 @@
+package com.glauco.estruturadados.array;
+
+import java.util.ArrayList;
+
+public class Aula12 {
+    public static void main(String[] args) {
+        ArrayList<String> arrayList = new ArrayList<String>();
+
+        arrayList.add("A");
+        arrayList.add("C");
+
+        System.out.println(arrayList);
+
+        arrayList.add(1, "B");
+
+        System.out.println(arrayList);
+
+        boolean existe = arrayList.contains("A");
+
+        if(existe) {
+            System.out.println("Elemento existe no array");
+        } else {
+            System.out.println("Elemento não existe no array");
+        }
+
+        int pos = arrayList.indexOf("B");
+        if (post > -1) {
+            System.out.println("Elemento existe no array");
+        } else {
+            System.out.println("Elemento não existe no array");
+        }
+
+        arrayList.remove(0);
+        arrayList.remove("B");
+
+        System.out.println(arrayList);
+        System.out.println(arrayList.size());
+    }
+}
