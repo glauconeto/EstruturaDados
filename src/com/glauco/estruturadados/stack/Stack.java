cedbcebef2638c3568f627742bc36bd123609ad0
glauconeto <glauco.neto@outlook.com>
2023-12-06 12:16:53 -0300

    Criadas classes de EstruturaEstatica e classe Pilha

---
+++ src/com/glauco/estruturadados/stack/Stack.java
@@ -0,0 +1,11 @@
+package com.glauco.estruturadados.stack;
+
+public class Stack<T> extends EstruturaEstatica<T> {
+    public Stack() {
+        super();
+    }
+
+    public Stack(int capacidade) {
+        super(capacidade);
+    }
+}
